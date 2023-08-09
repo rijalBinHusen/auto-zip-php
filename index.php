547d36c0697adfd38f52b5b8c7bace8d54f406fc
rijal <jeryzal333@gmail.com>
2023-08-09 20:15:49 +0700

    success to zip file only not folder

--- index.php
+++ index.php
@@ -2,109 +2,40 @@
 
 $black_list_to_not_show = array(".", "..", ".git");
 
-$dir = "./";
+$dir = ".";
 $directories = array();
+$zip_archive = new ZipArchive;
+
+// this should be contain date now and time
+$zip_file_name = "Ready to deploy.zip";
+
+$zip_archive_open = $zip_archive->open($zip_file_name, (ZipArchive::CREATE | ZipArchive::OVERWRITE));
+
+if ($zip_archive_open !== true) {
+  die("Faild to create archive!\n");
+}
+
 foreach (scandir($dir) as $file) {
   $is_on_black_list = in_array($file, $black_list_to_not_show);
+  $location = $dir . "/" . $file;
 
-  if($is_on_black_list) {
-  
+  if ($is_on_black_list) {
+
     continue;
+  } else if (is_dir($location)) {
 
+    // $zip_archive->addGlob($location . "/*");
+    // echo $location . "\n";
+  } else {
+    $zip_archive->addGlob($location);
+    // echo $location . "\n";
   }
 
-  else if (is_dir($dir . "/" . $file)) {
-    // $directories[] = $file;
-    echo $file . "\n";
+  $is_failed_to_zip = $zip_archive->status != ZipArchive::ER_OK;
 
-  } else {
-
-    echo $file . "\n";
-    
+  if ($is_failed_to_zip) {
+    echo "Failed to write " . $location . " files to zip\n";
   }
 }
 
-// foreach ($directories as $directory) {
-//   echo $directory . "\n";
-// }
-
-
-// $dir = "./";
-// $files = scandir($dir);
-// foreach ($files as $file) {
-//   if (!is_dir($dir . "/" . $file)) {
-//     echo $file . "\n";
-//   }
-// }
-
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-// class ZipHelper {
-
-//     /**
-//      * Add files and sub-directories in a folder to zip file.
-//      * @param string $folder
-//      * @param ZipArchive $zipFile
-//      * @param int $exclusiveLength Number of text to be exclusived from the file path.
-//      */
-//     private static function folderToZip($folder, &$zipFile, $exclusiveLength) {
-//         $handle = opendir($folder);
-//         while ($f = readdir($handle)) {
-//             if ($f != '.' && $f != '..') {
-//                 $filePath = "$folder/$f";
-//                 // Remove prefix from file path before add to zip.
-//                 $localPath = substr($filePath, $exclusiveLength);
-//                 if (is_file($filePath)) {
-//                     $zipFile->addFile($filePath, $localPath);
-//                 } elseif (is_dir($filePath)) {
-//                     // Add sub-directory.
-//                     $zipFile->addEmptyDir($localPath);
-//                     self::folderToZip($filePath, $zipFile, $exclusiveLength);
-//                 }
-//             }
-//         }
-//         closedir($handle);
-//     }
-
-//     /**
-//      * Zip a folder (include itself).
-//      * Usage:
-//      *   HZip::zipDir('/path/to/sourceDir', '/path/to/out.zip');
-//      *
-//      * @param string $sourcePath Path of directory to be zip.
-//      * @param string $outZipPath Path of output zip file.
-//      */
-//     public static function zipDir($sourcePath, $outZipPath) {
-//         $pathInfo = pathInfo($sourcePath);
-//         $parentPath = $pathInfo['dirname'];
-//         $dirName = $pathInfo['basename'];
-
-//         $z = new ZipArchive();
-//         $z->open($outZipPath, ZIPARCHIVE::CREATE);
-//         $z->addEmptyDir($dirName);
-//         self::folderToZip($sourcePath, $z, strlen("$parentPath/"));
-//         $z->close();
-//     }
-
-//     public static function getZipFile($dir) {
-//         $zipFiles = array();
-//         if (is_dir($dir)) {
-//             $objects = scandir($dir);
-
-//             foreach ($objects as $object) {
-//                 if ($object != "." && $object != "..") {
-//                     $path = $dir . "/" . $object;
-//                     if (filetype($path) != "dir") {
-//                         $info = pathinfo($path);
-//                         if (strtolower($info['extension']) == 'zip') {
-//                             $zipFiles[] = $object;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return $zipFiles;
-//     }
-
-// }
-?>
+$zip_archive->close();
