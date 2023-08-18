8919451f03bfeaf591f9810abc3a7615f0c1ec97
rijalBinHusen <jeryzal333@gmail.com>
2023-08-18 08:34:39 +0700

    all file and folder zipped

--- index.php
+++ index.php
@@ -1,66 +1,26 @@
 <?php
-
-// $black_list_to_not_show = array(".", "..", ".git");
-
-// $dir = ".";
-// $directories = array();
-// $zip_archive = new ZipArchive;
-
-// // this should be contain date now and time
-// $zip_file_name = "Ready to deploy.zip";
-
-// $zip_archive_open = $zip_archive->open($zip_file_name, (ZipArchive::CREATE | ZipArchive::OVERWRITE));
-
-// if ($zip_archive_open !== true) {
-//   die("Faild to create archive!\n");
-// }
-
-// foreach (scandir($dir) as $file) {
-//   $is_on_black_list = in_array($file, $black_list_to_not_show);
-//   $location = $dir . "/" . $file;
-
-//   if ($is_on_black_list) {
-
-//     continue;
-//   } else if (is_dir($location)) {
-
-//     // $zip_archive->addGlob($location . "/*");
-//     // echo $location . "\n";
-//   } else {
-//     $zip_archive->addGlob($location);
-//     // echo $location . "\n";
-//   }
-
-//   $is_failed_to_zip = $zip_archive->status != ZipArchive::ER_OK;
-
-//   if ($is_failed_to_zip) {
-//     echo "Failed to write " . $location . " files to zip\n";
-//   }
-// }
-
-// $zip_archive->close();
-
 // ============== This function is working very well
 
 function getDirContents($dir, &$results = array()) {
-    $black_list_to_not_show = array(".", "..", ".git");
+    $black_list_to_not_zip = array(".", "..", ".git");
+    // // this should be contain date now and time
 
     $files = scandir($dir);
 
     foreach ($files as $file) {
-      $is_on_black_list = in_array($file, $black_list_to_not_show);
+      $is_on_black_list = in_array($file, $black_list_to_not_zip);
 
         $location = $dir . "/" . $file;
         
         if (!is_dir($location)) {
 
-            $results[] = $location;
-
+          
+          $results[] = $location;
         } else if ($is_on_black_list === false) {
 
-            getDirContents($location, $results);
-
-            $results[] = $location;
+          $results[] = $location;
+          getDirContents($location, $results);
+          // $results[] = $location;
 
         }
     }
@@ -69,5 +29,39 @@ function getDirContents($dir, &$results = array()) {
 
 }
 
+function zip_all_file_and_folder () {
 
-var_dump(getDirContents("."));
+  $locations = getDirContents(".");
+
+  $zip_archive = new ZipArchive;
+
+  // this should be contain date now and time
+  $zip_file_name = "Ready_to_deploy.zip";
+
+  $zip_archive_open = $zip_archive->open($zip_file_name, (ZipArchive::CREATE | ZipArchive::OVERWRITE));
+
+  if ($zip_archive_open !== true) {
+    die("Faild to create archive!\n");
+  }
+
+  foreach ($locations as $location) {
+
+    if (is_dir($location)) {
+
+      $zip_archive->addEmptyDir($location);
+    } else {
+
+      $zip_archive->addGlob($location);
+    }
+
+    $is_failed_to_zip = $zip_archive->status != ZipArchive::ER_OK;
+
+    if ($is_failed_to_zip) {
+      echo "Failed to write " . $location . " files to zip\n";
+    }
+  }
+
+  $zip_archive->close();
+}
+
+zip_all_file_and_folder();
